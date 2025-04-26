e8298ffa28549206856d03246fe25e38a2d13ae8
MertCtn <mertctnstd35@gmail.com>
2025-04-26 10:03:25 +0300

    SubmissionThread class defined with its fields and constructor

--- app/src/main/java/ce316project/utils/SubmissionThread.java
+++ app/src/main/java/ce316project/utils/SubmissionThread.java
@@ -1,5 +1,29 @@
 package ce316project.utils;
 
+import ce316project.entities.CompilationResult;
+import ce316project.entities.ExecutionResult;
+
 public class SubmissionThread {
+    private ExecutionResult executionResult;
+    private CompilationResult compilationResult;
+    private Status status;
+
+    public SubmissionThread(ExecutionResult executionResult, CompilationResult compilationResult, Status status) {
+        this.executionResult = executionResult;
+        this.compilationResult = compilationResult;
+        this.status = status;
+    }
     
+    /* 
+    public CompilationResult compile()
+    {
+        
+    }
+
+    public ExecutionResult execute()
+    {
+        
+    }
+    */
+
 }
