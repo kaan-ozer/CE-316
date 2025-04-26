d005898bb91f574a33a9eec508dadede9ea26df2
MertCtn <mertctnstd35@gmail.com>
2025-04-26 09:30:05 +0300

    Added definition of Project Manager class

--- app/src/main/java/ce316project/utils/ProjectManager.java
+++ app/src/main/java/ce316project/utils/ProjectManager.java
@@ -1,5 +1,30 @@
 package ce316project.utils;
 
+import java.util.List;
+
+import ce316project.entities.Project;
+
 public class ProjectManager {
+        private List<Project> projects;
+    private Path projectsDir;
     
+    void createProject()
+    {
+
+    }
+
+    void openProject(String projectId)
+    {
+
+    }
+
+    void loadAllProjects(String filePath)
+    {
+
+    }
+
+    void saveProject(Project project)
+    {
+
+    }
 }
