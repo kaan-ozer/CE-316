cbf3dcfb6306256b8d47d13b27a0c3ba58ec30fc
MertCtn <mertctnstd35@gmail.com>
2025-04-26 09:42:28 +0300

    Status enum added

---
+++ app/src/main/java/ce316project/entities/ExecutionResult.java
@@ -0,0 +1,5 @@
+package ce316project.entities;
+
+public class ExecutionResult {
+    
+}
---
+++ app/src/main/java/ce316project/utils/Status.java
@@ -0,0 +1,10 @@
+package ce316project.utils;
+
+public enum Status{
+    RECEIVED,
+    COMPILING,
+    READY,
+    EXECUTING,
+    COMPLETED,
+    ERROR
+}
