c9285e6d1b8b6c67f8613187f0d294779c5ce572
MertCtn <mertctnstd35@gmail.com>
2025-04-26 10:05:58 +0300

    Configuration class defined with its fields and constructor

--- app/src/main/java/ce316project/entities/Configuration.java
+++ app/src/main/java/ce316project/entities/Configuration.java
@@ -1,5 +1,27 @@
 package ce316project.entities;
 
+import java.nio.file.Path;
+import java.util.List;
+
 public class Configuration {
-    
+
+    private String configId;
+    private String language;
+    private Path compilerPath;
+    private String compilerCommand;
+    private List<String> compilerParameters;
+    private String runCommand;
+    private List<String> runParameters;
+
+    public Configuration(String configId, String language, Path compilerPath, String compilerCommand,
+            List<String> compilerParameters, String runCommand, List<String> runParameters) {
+        this.configId = configId;
+        this.language = language;
+        this.compilerPath = compilerPath;
+        this.compilerCommand = compilerCommand;
+        this.compilerParameters = compilerParameters;
+        this.runCommand = runCommand;
+        this.runParameters = runParameters;
+    }
+
 }
